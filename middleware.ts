4e86ce33f2cd6654dfe69590ef76cc3599968134
Seya Weber <swbr@proton.me>
2025-09-19 10:08:37 +0200

    Fix middleware matcher and update pnpm lock

--- middleware.ts
+++ middleware.ts
@@ -64,5 +64,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/(?!_next/static|_next/image|favicon.ico).+"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 }
